686b3c15018452c6daa358d3c2fb66d8329b642c
Han -Ju - Hwan - PC <Han -Ju - Hwan - PC@Han-Ju-Hwan-PC>
2020-12-27 14:37:16 +0900

    (JDK, STS, Tomcat) version and etc configuration finished

--- jex00/src/main/java/org/zerock/config/WebConfig.java
+++ jex00/src/main/java/org/zerock/config/WebConfig.java
@@ -1,5 +1,24 @@
 package org.zerock.config;
 
-public class WebConfig {
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer{
+	
+	@Override
+	protected Class<?>[] getRootConfigClasses(){
+		// TODO Auto-generated method stub
+		return new Class[] {RootConfig.class};
+	}
+	
+	@Override
+	protected Class<?>[] getServletConfigClasses(){
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	protected String[] getServletMappings(){
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
