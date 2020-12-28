cffe787d2087b811fd8819726080c39282b10c7b
LuneRemer <europejumin@gmail.com>
2020-12-28 18:06:53 +0900

    Sample Test(Spring framework)

--- ex00/src/main/java/org/zerock/sample/Restaurant.java
+++ ex00/src/main/java/org/zerock/sample/Restaurant.java
@@ -6,8 +6,12 @@ import org.springframework.stereotype.Component;
 import lombok.Data;
 import lombok.Setter;
 
+//@Component 표시가 있는 것들만 인스턴스 생성(root-context에서 base package)
 @Component
+
+//Lombok을 이용해서 여러 메서드가 만들어짐.(getter, setter, onMethod_, 등등)
 @Data
+
 public class Restaurant {
 	
 	@Setter(onMethod_ = @Autowired)
