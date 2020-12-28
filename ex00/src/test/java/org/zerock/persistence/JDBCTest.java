d3fd33d05c347692c4c25a3f6dbccedf317508ee
LuneRemer <europejumin@gmail.com>
2020-12-29 00:36:05 +0900

    Explanation added

--- ex00/src/test/java/org/zerock/persistence/JDBCTest.java
+++ ex00/src/test/java/org/zerock/persistence/JDBCTest.java
@@ -2,11 +2,11 @@ package org.zerock.persistence;
 
 import static org.junit.Assert.fail;
 
+//Libraries for Connection Test
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 import org.junit.Test;
-
 
 import lombok.extern.log4j.Log4j;
 
@@ -24,9 +24,9 @@ public class JDBCTest {
 	public void testConnection(){
 		try(Connection con = 
 				DriverManager.getConnection(
-						"jdbc:oracle:thin:@localhost:1521:XE",
-						"book_ex",
-						"book_ex")){
+						"jdbc:oracle:thin:@localhost:1521:XE", // jdbc:oracle:thin:@접속이름:포트:SID
+						"book_ex", // User Name
+						"book_ex")){ // Password
 			
 			log.info(con);
 		} catch(Exception e) {
