5c78dc2543e83ca29badeaf009dc1fb43399e21c
LuneRemer <europejumin@gmail.com>
2020-12-29 17:40:02 +0900

    TimeMapper Interface 와 XML을 같이 이용하는 방법(getTime2:xml이용, getTime은 이용하지 않음)

--- ex00/src/main/java/org/zerock/mapper/TimeMapper.java
+++ ex00/src/main/java/org/zerock/mapper/TimeMapper.java
@@ -7,4 +7,7 @@ public interface TimeMapper {
 	//@Select : MyBatis의 Annotation
 	@Select("SELECT sysdate FROM dual")
 	public String getTime();
+	
+	//SQL 구문이 없는 이유 : XML파일을 이용해서 처리할것이기 때문임.
+	public String getTime2();
 }
